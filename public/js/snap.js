4a0dce838b089b184ce9d4f6eb6dc53f4d15557d
陈亮 <chenliang@aldwx.com>
2018-11-25 02:14:20 +0800

    8秒

--- public/js/snap.js
+++ public/js/snap.js
@@ -4,14 +4,9 @@ var args = require('system').args;
 var url = args[1];
 var filename = args[2];
 
-page.open(url, function (status) {
-
-    if ( status === "success" ) {
-        setTimeout(function(){
-            page.render(filename);
-        },500);
-    } else {
-        console.log("Page failed to load.");
-    }
-    phantom.exit();
+page.open(url, function () {
+    setTimeout(function(){
+        page.render(filename);
+        phantom.exit();
+    },8000);
 });
